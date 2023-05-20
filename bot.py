639a702ab8b6eb5f09774c2ac1c1faff7fd9ff37
Miki <104788529+Mikilled@users.noreply.github.com>
2023-05-20 15:00:00 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,184 @@
+import os
+import zipfile
+from telebot import *
+from docxcompose.composer import Composer
+from docx import Document as Document_compose
+from parstitul import pars_titul
+from writetitul import write_titul
+from pocessinginfile import process_document
+import time as tm
+from datetime import date
+token = '6295000781:AAHo4pljJ1GmkRXRier4-ckD16n8sD5rCjo'
+bot = telebot.TeleBot(token)
+
+
+
+
+spams = {}
+
+
+def is_spam(user_id, spams, msgs, max, ban, msg):
+    try:
+        usr = spams[user_id]
+        usr["messages"] += 1
+    except:
+        spams[user_id] = {"next_time": int(tm.time()) + max, "messages": 1, "banned": 0}
+        usr = spams[user_id]
+    if usr["banned"] >= int(tm.time()):
+        print(u'' + str((datetime.datetime.today()).strftime("%H:%M ")) + f"спамит {user_id}")
+        return False
+    else:
+        if usr["next_time"] >= int(tm.time()):
+            if usr["messages"] >= msgs:
+                spams[user_id]["banned"] = tm.time() + ban
+                if not (msg == ''):
+                    text = f"{msg}{int(ban)} секунд"
+                    bot.send_message(user_id, text)
+                return False
+        else:
+            spams[user_id]["messages"] = 1
+            spams[user_id]["next_time"] = int(tm.time()) + max
+    return True
+
+
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    upl = types.KeyboardButton("Загрузить отчет")
+    inf = types.KeyboardButton("о боте")
+    markup.add(upl,inf)
+    print(message.chat.username)
+    text = """
+    Бот не делает:
+    -Пишет за вас отчет
+    -Заполняет не достающими данными (название картинок и тд.)
+    -Не проверяет работу
+    Бат делает:
+    -Правильно оформляет титульный лист
+    -Выставляет правильные шрифты, отступы
+    -Находит ошибки оформления
+    -Оформляет документы в формате docx
+    """
+    bot.send_message(message.chat.id, text, reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def actions(message):
+    if not is_spam(message.chat.id, spams, 10, 10, 10, "бан на  "):
+        return
+    text = """
+        *ВНИМАНИЕ!!!*
+        Мини гайд как заполнить отчет:
+        - Заголовки должны быть выделены *жирным* шрифтом
+        - Все картинки должны иметь подпись \n          (Рисунок 1 – ER-диаграмма)
+        - Все таблицы должны иметь заголовок \n         (Таблица 1 - Определение сущностей)
+        - Титульный лист должен содержать город \n          (Новосибирск 2023)
+    """
+    if message.text == "Загрузить отчет":
+        bot.send_message(message.chat.id, text, parse_mode='Markdown')
+    if message.text == "о боте":
+        text1 = text + """
+            Бот не делает:
+            -Пишет за вас отчет
+            -Заполняет не достающими данными (название картинок и тд.)
+            -Не проверяет работу
+            Бат делает:
+            -Правильно оформляет титульный лист
+            -Выставляет правильные шрифты, отступы
+            -Находит ошибки оформления
+            - и тд.
+            """
+        bot.send_message(message.chat.id, text1, parse_mode='Markdown')
+
+
+
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+    if not is_spam(message.chat.id, spams, 1, 15, 10, "Отправлять можно раз в "):
+        return
+    file_id = message.document.file_id
+    file_info = bot.get_file(file_id)
+    file_path = file_info.file_path
+
+
+    downloaded_file = bot.download_file(file_path)
+    print(message.chat.username)
+    if not os.path.exists("docx"):
+        os.makedirs("docx")
+    with open(f'docx/{message.document.file_id}.docx', 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    try:
+        count, info = pars_titul(f'docx/{message.document.file_id}.docx')
+        print("спарсил титул")
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, 'Ошибка парсинга титульника')
+        del_files(message.document.file_id)
+        return
+    try:
+        outdoc = write_titul(f'docx/titul-{message.document.file_id}.docx', info)
+        outdoc.save(f'docx/titul-{message.document.file_id}.docx')
+        print("Записал титул")
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, 'Ошибка запис')
+        del_files(message.document.file_id)
+        return
+
+    try:
+        new_doc,errors = process_document(f'docx/{message.document.file_id}.docx', count)
+        new_doc.save(f'docx/text-{message.document.file_id}.docx')
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, 'Ошибка обработки документа')
+        del_files(message.document.file_id)
+        return
+
+    try:
+        master = Document_compose(f'docx/titul-{message.document.file_id}.docx')
+        composer = Composer(master)
+        doc2 = Document_compose(f'docx/text-{message.document.file_id}.docx')
+        composer.append(doc2)
+        composer.save(f'docx/new{message.document.file_id}.docx')
+    except Exception as e:
+        print(e)
+        bot.send_message(message.chat.id, 'Ошибка склейки документов')
+        del_files(message.document.file_id)
+        return
+    mail = ''
+    if len(errors) != 0:
+        mail = '\n'.join(errors)
+    with open(f'docx/new{message.document.file_id}.docx', 'rb') as new_file:
+        bot.send_document(message.chat.id, new_file, caption=mail)
+
+    if not os.path.exists("zip"):
+        os.makedirs("zip")
+
+    with zipfile.ZipFile(rf'zip/docum.zip', 'w') as myzip:
+        myzip.write(rf'docx/{message.document.file_id}.docx', compress_type=zipfile.ZIP_DEFLATED, arcname=f"{message.from_user.username}")
+
+    del_files(message.document.file_id)
+
+def del_files(name):
+    try:
+        os.remove(f'docx/titul-{name}.docx')
+    except:
+        pass
+    try:
+        os.remove(f'docx/text-{name}.docx')
+    except:
+        pass
+    try:
+        os.remove(f'docx/new{name}.docx')
+    except:
+        pass
+    try:
+        os.remove(f'docx/{name}.docx')
+    except:
+        pass
+
+
+bot.polling(none_stop=True)
---
+++ writetitul.py
@@ -0,0 +1,237 @@
+import re
+import docx
+from docx.enum.table import WD_CELL_VERTICAL_ALIGNMENT
+from docx.shared import Pt
+from docx.shared import Cm
+from docx.shared import Inches
+import calendar
+
+import datetime
+import locale
+from parstitul import pars_titul
+locale.setlocale(
+    category=locale.LC_ALL,
+    locale="Russian"
+)
+
+
+def write_titul(name_file,info):
+    name_vuz = 'МИНИСТЕРСТВО НАУКИ И ВЫСШЕГО ОБРАЗОВАНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ\n\nФЕДЕРАЛЬНОЕ ГОСУДАРСТВЕННОЕ БЮДЖЕТНОЕ\nОБРАЗОВАТЕЛЬНОЕ УЧРЕЖДЕНИЕ\nВЫСШЕГО ОБРАЗОВАНИЯ\n«НОВОСИБИРСКИЙ ГОСУДАРСТВЕННЫЙ ТЕХНИЧЕСКИЙ УНИВЕРСИТЕТ»\n___________________________________________________________________________'
+    doc = docx.Document()
+
+    p = doc.add_paragraph()
+
+    section = doc.sections[0]
+
+    section.left_margin = Cm(2.54)
+    section.right_margin = Cm(2.54)
+    #section.top_margin = Cm(2.54)
+    #section.bottom_margin = Cm(2.54)
+
+
+
+
+    run = p.add_run(name_vuz )
+    p.alignment = 1 # for left, 1 for center, 2 right, 3 justify
+    font = run.font
+    font.name = 'Times New Roman'
+    font.size = Pt(12)
+
+    run = p.add_run(f'\n{info["факультет"]}\n{info["кафедра"]}\n\n')
+    font = run.font
+    font.name = 'Times New Roman'
+    font.size = Pt(14)
+
+    picture = run.add_picture("nstu_logo.png", width=Inches(3))
+
+
+    p = doc.add_paragraph()
+    p.paragraph_format.line_spacing = 1.3
+
+    p.alignment = 1
+    run = p.add_run(f'ОТЧЁТ\n')
+    font = run.font
+    font.name = 'Times New Roman'
+    font.size = Pt(16)
+    font.bold = True
+
+
+    run = p.add_run(f'{info["тип"]}\n\n')
+    font = run.font
+    font.name = 'Times New Roman'
+    font.size = Pt(14)
+    font.bold = True
+
+
+    run = p.add_run(f'{info["тема"]}\n')
+    font = run.font
+    font.name = 'Times New Roman'
+    font.size = Pt(14)
+    font.italic = True
+
+
+    run = p.add_run('по дисциплине: ')
+    run.font.name = 'Times New Roman'
+    run.font.size = Pt(14)
+    run.font.bold = True
+
+    run = p.add_run(f'{info["дисциплина"]}')
+    run.font.name = 'Times New Roman'
+    run.font.size = Pt(14)
+    run.font.italic = True
+
+    if info["вариант"]:
+        run = p.add_run(f'\n{info["вариант"]}\n')
+        run.font.name = 'Times New Roman'
+        run.font.size = Pt(14)
+    else:
+        run = p.add_run('\n')
+
+    from docx.table import _Cell
+    from docx.oxml import OxmlElement
+    from docx.oxml.ns import qn
+
+
+    def set_cell_border(cell: _Cell, **kwargs): #https://www.programmersought.com/article/74085524416/
+
+        tc = cell._tc
+        tcPr = tc.get_or_add_tcPr()
+
+
+        tcBorders = tcPr.first_child_found_in("w:tcBorders")
+        if tcBorders is None:
+            tcBorders = OxmlElement('w:tcBorders')
+            tcPr.append(tcBorders)
+
+
+        for edge in ('start', 'top', 'end', 'bottom', 'insideH', 'insideV'):
+            edge_data = kwargs.get(edge)
+            if edge_data:
+                tag = 'w:{}'.format(edge)
+
+
+                element = tcBorders.find(qn(tag))
+                if element is None:
+                    element = OxmlElement(tag)
+                    tcBorders.append(element)
+
+
+                for key in ["sz", "val", "color", "space", "shadow"]:
+                    if key in edge_data:
+                        element.set(qn('w:{}'.format(key)), str(edge_data[key]))
+
+
+    table = doc.add_table(rows=2, cols=2)
+    table.style = 'Table Grid'
+
+
+    cell1 = table.cell(0, 0)
+    cell1.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.TOP
+    set_cell_border(
+    cell1,
+    top={"sz": 1,"color": "#FFFFFF"},
+    bottom={"sz": 1, "color": "#FFFFFF"},
+    start={"sz": 1, "color": "#FFFFFF"},
+    end={"sz": 1, "color": "#FFFFFF"},
+    )
+    cell2 = table.cell(0, 1)
+    set_cell_border(
+    cell2,
+    top={"sz": 1,"color": "#FFFFFF"},
+    bottom={"sz": 1, "color": "#FFFFFF"},
+    start={"sz": 1, "color": "#FFFFFF"},
+    end={"sz": 1, "color": "#FFFFFF"},
+    )
+
+    cell3 = table.cell(1, 1)
+    set_cell_border(
+    cell3,
+    top={"sz": 1,"color": "#FFFFFF"},
+    bottom={"sz": 1, "color": "#FFFFFF"},
+    start={"sz": 1, "color": "#FFFFFF"},
+    end={"sz": 1, "color": "#FFFFFF"},
+    )
+
+    cell4 = table.cell(1, 0)
+    set_cell_border(
+    cell4,
+    top={"sz": 1,"color": "#FFFFFF"},
+    bottom={"sz": 1, "color": "#FFFFFF"},
+    start={"sz": 1, "color": "#FFFFFF"},
+    end={"sz": 1, "color": "#FFFFFF"},
+    )
+
+    cell1_text = cell1.add_paragraph(f'Выполнил(а):\nСтудент гр. {info["группа"]}\n{info["выполнил"]}')
+    cell1_text.style.font.name = 'Times New Roman'
+    cell1_text.style.font.size = Pt(12)
+    cell1_text.paragraph_format.space_before = 0
+    cell1_text.paragraph_format.space_after = 0
+
+
+
+    cell2_text = cell2.add_paragraph(f'\t\tПроверил:\n\n\t\t{info["проверил"]}')
+    cell2_text.style.font.name = 'Times New Roman'
+    cell2_text.style.font.size = Pt(12)
+    cell2_text.paragraph_format.space_before = 0
+    cell2_text.paragraph_format.space_after = 0
+
+
+
+
+
+    cell3_text = cell3.add_paragraph(f'\t\t«___» ______ {datetime.now().strftime("%Y")}г\n\t\t_________________\n\t\t(подпись)')
+    cell3_text.style.font.name = 'Times New Roman'
+    cell3_text.style.font.size = Pt(13)
+    cell3_text.paragraph_format.space_before = 0
+    cell3_text.paragraph_format.space_after = 0
+
+
+
+
+
+    cell4_text = cell4.add_paragraph(f'«{datetime.now().strftime("%d")}» {calendar.month_name[datetime.now().month].replace("ь", "я")} {datetime.now().strftime("%Y")}г\n_________________\n(подпись)')
+    cell4_text.style.font.name = 'Times New Roman'
+    cell4_text.style.font.size = Pt(13)
+    cell4_text.paragraph_format.space_before = 0
+    cell4_text.paragraph_format.space_after = 0
+
+
+    doc.add_paragraph()
+    p = doc.add_paragraph()
+    p.alignment = 1
+    run = p.add_run(f'Новосибирск {datetime.now().strftime("%Y")}г.')
+    font = run.font
+    font.name = 'Times New Roman'
+    font.size = Pt(12)
+    # doc.add_page_break()
+    doc.save(name_file)
+
+
+    return  doc
+
+
+if __name__ == '__main__':
+    count,info =pars_titul(r"C:\Users\admin\Downloads\Otchyot.docx")
+    print(count)
+    doc = write_titul("testwrite.docx",info)
+    doc.save('testwrite.docx')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
